783b9b0e01335f8623c1b6506be3999a5f09c5cc
Evaldas Naujanis <evaldas.naujanis@visma.com>
2022-02-02 23:30:45 +0200

    AoC#0: Main project added

---
+++ AdventOfCode-2021/AdventOfCode.MainApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace AdventOfCode.MainApp
+{
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var uiMode = 0;
+
+            switch (uiMode)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    }
+
+}
+
