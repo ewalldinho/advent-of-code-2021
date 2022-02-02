5375e28848b5b37cd3ac399b2cb0ae3bc22a07ad
Evaldas Naujanis <evaldas.naujanis@visma.com>
2022-02-03 01:42:06 +0200

    AoC#17: Day 17 - Trick Shot. Input data, C# solution, unit tests

---
+++ AdventOfCode-2021/AdventOfCode.Csharp.Tests/Day17Tests.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.Csharp.Solutions;
+using Xunit;
+
+namespace AdventOfCode.Csharp.Tests
+{
+    public class Day17Tests
+    {
+        private readonly Day17 _day17Solution = new();
+
+        [Fact]
+        public void CorrectlyCalculatesSolutionPart1()
+        {
+            var inputData = GetTestData(0);
+
+            var maxHeight = _day17Solution.CalculateSolution(Parts.Part1, inputData);
+
+            Assert.Equal("45", maxHeight);
+        }
+
+        [Fact]
+        public void CorrectlyCalculatesSolutionPart2_After40Steps()
+        {
+            var inputData = GetTestData(0);
+
+            var expressionValue = _day17Solution.CalculateSolution(Parts.Part2, inputData);
+
+            Assert.Equal("112", expressionValue);
+        }
+
+
+        private static string GetTestData(int testCase)
+        {
+            return testCase switch
+            {
+                0 => "target area: x=20..30, y=-10..-5",
+                1 => "", 
+                _ => ""
+            };
+
+        }
+
+
+    }
+}
---
+++ AdventOfCode-2021/AdventOfCode.Csharp/Solutions/Day17.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Csharp.Solutions
+{
+    public class Day17 : IPuzzle
+    {
+        public string CalculateSolution(Parts part, string inputData)
+        {
+            var targetArea = ParseInput(inputData);
+
+            return part switch
+            {
+                Parts.Part1 => $"{SolvePart1(targetArea)}",
+                Parts.Part2 => $"{SolvePart2(targetArea)}",
+                _ => throw new ArgumentOutOfRangeException(nameof(part), part, "There are only 2 parts.")
+            };
+        }
+        
+        private static int SolvePart1(TargetArea targetArea)
+        {
+            var highestPoints = new Dictionary<(int, int), int?>();
+            var b2 = 1 + 4 * 2 * targetArea.FromX;
+            // we need only positive speed value
+            var minHorizontalSpeed = (-1 + (int)Math.Sqrt(b2)) / 2;
+            var maxHeight = 0;
+            var hitStatus = 0;  // 0 - not hit yet, 1 - has hit, 2 - overshot
+            for (var speedX = minHorizontalSpeed; speedX <= targetArea.ToX; speedX++)
+            {
+                for (var speedY = -100; speedY < 1000; speedY++)
+                {
+                    var height = SimulateProbeLaunch(targetArea, speedX, speedY);
+                    if (height <= 0)
+                    {
+                        if (hitStatus == 1)
+                        {
+                            hitStatus++;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (hitStatus == 0)
+                            hitStatus++;
+                    }
+                    
+                    if (height > maxHeight)
+                    {
+                        maxHeight = height;
+                    }
+                    highestPoints.Add((speedX, speedY), height);
+                }
+            }
+
+            var hitTargets = highestPoints.Where(kvp => kvp.Value > 0).ToList();
+            var hitTarget = highestPoints.Where(kvp => kvp.Value == maxHeight).ToList();
+            return maxHeight;
+        }
+
+        private static int SolvePart2(TargetArea targetArea)
+        {
+            var hitPoints = new Dictionary<(int, int), bool>();
+            var b2 = 1 + 4 * 2 * targetArea.FromX;
+            // we need only positive speed value
+            var minHorizontalSpeed = (-1 + (int)Math.Sqrt(b2)) / 2;
+            //var maxHeight = 0;
+            //var hitStatus = 0;  // 0 - not hit yet, 1 - has hit, 2 - overshot
+            for (var speedX = minHorizontalSpeed; speedX <= targetArea.ToX; speedX++)
+            {
+                for (var speedY = -10000; speedY < 10000; speedY++)
+                {
+                    var hasHitTarget = SimulateProbeLaunchUntilHit(targetArea, speedX, speedY);
+
+                    hitPoints.Add((speedX, speedY), hasHitTarget);
+                }
+            }
+
+            var hits = hitPoints.Where(kvp => kvp.Value).ToList();
+            return hits.Count;
+        }
+        
+        private class TargetArea
+        {
+            public int FromX { get; set; }
+            public int ToX { get; set; }
+            public int FromY { get; set; }
+            public int ToY { get; set; }
+        }
+
+        private static TargetArea ParseInput(string inputData)
+        {
+            var targetArea = inputData.Split(": ").Last()
+                .Split(", ").Select(coords => coords.Split('='))
+                .ToDictionary(coordinate => coordinate[0], coordinate => coordinate[1].Split("..").Select(int.Parse).ToArray());
+            return new TargetArea
+            {
+                FromX = targetArea["x"][0],
+                ToX = targetArea["x"][1],
+                FromY = targetArea["y"][0],
+                ToY = targetArea["y"][1]
+            };
+        }
+        
+        private static int SimulateProbeLaunch(TargetArea targetArea, int speedX, int speedY)
+        {
+            var maxHeight = 0;
+            var posX = 0;
+            var posY = 0;
+            var speedHorizontal = speedX;
+            var speedVertical = speedY;
+            var hasMissedTarget = false;
+            var isWithinTargetArea = false;
+            while (!(isWithinTargetArea || hasMissedTarget))
+            {
+                posX += speedHorizontal;
+                posY += speedVertical;
+                if (posY > maxHeight)
+                    maxHeight = posY;
+
+                if (speedHorizontal > 0)
+                    speedHorizontal--;
+                speedVertical--;
+
+                isWithinTargetArea = targetArea.FromX <= posX && posX <= targetArea.ToX && targetArea.FromY <= posY && posY <= targetArea.ToY;
+                hasMissedTarget = posX > targetArea.ToX || posY < targetArea.FromY;
+            }
+
+            if (isWithinTargetArea)
+                return (speedY * speedY + speedY) / 2;
+            if (hasMissedTarget)
+                return 0;
+            return -1;
+        }
+
+        private static bool SimulateProbeLaunchUntilHit(TargetArea targetArea, int speedX, int speedY)
+        {
+            var maxHeight = 0;
+            var posX = 0;
+            var posY = 0;
+            var speedHorizontal = speedX;
+            var speedVertical = speedY;
+            var hasMissedTarget = false;
+            var isWithinTargetArea = false;
+            while (!(isWithinTargetArea || hasMissedTarget))
+            {
+                posX += speedHorizontal;
+                posY += speedVertical;
+                if (posY > maxHeight)
+                    maxHeight = posY;
+
+                if (speedHorizontal > 0)
+                    speedHorizontal--;
+                speedVertical--;
+
+                isWithinTargetArea = targetArea.FromX <= posX && posX <= targetArea.ToX && targetArea.FromY <= posY && posY <= targetArea.ToY;
+                hasMissedTarget = posX > targetArea.ToX || posY < targetArea.FromY;
+            }
+
+            if (isWithinTargetArea)
+                return true;
+
+            return hasMissedTarget && false;
+        }
+
+
+    }
+}
