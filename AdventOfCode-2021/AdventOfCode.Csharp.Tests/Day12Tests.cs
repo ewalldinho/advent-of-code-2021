3489332b384e13ccb8b6dac7939ecbc9e91f7916
Evaldas Naujanis <evaldas.naujanis@visma.com>
2022-02-03 00:38:47 +0200

    AoC#12: Day 12: Passage Pathing. Input data, C# solution and tests

---
+++ AdventOfCode-2021/AdventOfCode.Csharp.Tests/Day12Tests.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode.Csharp.Solutions;
+using Xunit;
+
+namespace AdventOfCode.Csharp.Tests
+{
+    public class Day12Tests
+    {
+        private readonly Day12 _day12Solution = new();
+
+
+        [Fact]
+        public void CaveSystem_CorrectlyFindsAllPaths()
+        {
+            var inputData = GetTestData(0);
+            var cavePairs = inputData.Split(Environment.NewLine).Select(line => line.Split('-')).ToList();
+            var caveSystem = new Day12.CaveSystem(cavePairs);
+
+            var allPaths = caveSystem.FindAllPaths();
+
+            Assert.Equal(10, allPaths.Count);
+            Assert.Contains("start,A,b,A,c,A,end", allPaths);
+            Assert.Contains("start,A,b,A,end", allPaths);
+            Assert.Contains("start,A,b,end", allPaths);
+            Assert.Contains("start,A,c,A,b,A,end", allPaths);
+            Assert.Contains("start,A,c,A,b,end", allPaths);
+            Assert.Contains("start,A,c,A,end", allPaths);
+            Assert.Contains("start,A,end", allPaths);
+            Assert.Contains("start,b,A,c,A,end", allPaths);
+            Assert.Contains("start,b,A,end", allPaths);
+            Assert.Contains("start,b,end", allPaths);
+        }
+
+        [Fact]
+        public void CaveSystem_CorrectlyFindsAllPathsWithExtraVisit()
+        {
+            var inputData = GetTestData(0);
+            var cavePairs = inputData.Split(Environment.NewLine).Select(line => line.Split('-')).ToList();
+            var caveSystem = new Day12.CaveSystem(cavePairs);
+
+            var allPaths = caveSystem.FindAllPathsWithExtraVisit();
+            
+            Assert.Contains("start,A,b,A,b,A,c,A,end", allPaths);
+            Assert.Contains("start,A,b,A,b,A,end", allPaths);
+            Assert.Contains("start,A,b,A,b,end", allPaths);
+            Assert.Contains("start,A,b,A,c,A,b,A,end", allPaths);
+            Assert.Contains("start,A,b,A,c,A,b,end", allPaths);
+            Assert.Contains("start,A,b,A,c,A,c,A,end", allPaths);
+            Assert.Contains("start,A,b,A,c,A,end", allPaths);
+            Assert.Contains("start,A,b,A,end", allPaths);
+            Assert.Contains("start,A,b,d,b,A,c,A,end", allPaths);
+            Assert.Contains("start,A,b,d,b,A,end", allPaths);
+            Assert.Contains("start,A,b,d,b,end", allPaths);
+            Assert.Contains("start,A,b,end", allPaths);
+            Assert.Contains("start,A,c,A,b,A,b,A,end", allPaths);
+            Assert.Contains("start,A,c,A,b,A,b,end", allPaths);
+            Assert.Contains("start,A,c,A,b,A,c,A,end", allPaths);
+            Assert.Contains("start,A,c,A,b,A,end", allPaths);
+            Assert.Contains("start,A,c,A,b,d,b,A,end", allPaths);
+            Assert.Contains("start,A,c,A,b,d,b,end", allPaths);
+            Assert.Contains("start,A,c,A,b,end", allPaths);
+            Assert.Contains("start,A,c,A,c,A,b,A,end", allPaths);
+            Assert.Contains("start,A,c,A,c,A,b,end", allPaths);
+            Assert.Contains("start,A,c,A,c,A,end", allPaths);
+            Assert.Contains("start,A,c,A,end", allPaths);
+            Assert.Contains("start,A,end", allPaths);
+            Assert.Contains("start,b,A,b,A,c,A,end", allPaths);
+            Assert.Contains("start,b,A,b,A,end", allPaths);
+            Assert.Contains("start,b,A,b,end", allPaths);
+            Assert.Contains("start,b,A,c,A,b,A,end", allPaths);
+            Assert.Contains("start,b,A,c,A,b,end", allPaths);
+            Assert.Contains("start,b,A,c,A,c,A,end", allPaths);
+            Assert.Contains("start,b,A,c,A,end", allPaths);
+            Assert.Contains("start,b,A,end", allPaths);
+            Assert.Contains("start,b,d,b,A,c,A,end", allPaths);
+            Assert.Contains("start,b,d,b,A,end", allPaths);
+            Assert.Contains("start,b,d,b,end", allPaths);
+            Assert.Contains("start,b,end", allPaths);
+        }
+
+        [Fact]
+        public void CaveSystem_CorrectlyCountsPaths()
+        {
+            var inputData = GetTestData(0);
+            var cavePairs = inputData.Split(Environment.NewLine).Select(line => line.Split('-')).ToList();
+            var caveSystem = new Day12.CaveSystem(cavePairs);
+
+            var actualPathsCount = caveSystem.CountPathsThroughCaveSystem();
+
+            Assert.Equal(10, actualPathsCount);
+        }
+
+        [Fact]
+        public void CaveSystem_CorrectlyCountsPathsWithExtraVisit()
+        {
+            var inputData = GetTestData(0);
+            var cavePairs = inputData.Split(Environment.NewLine).Select(line => line.Split('-')).ToList();
+            var caveSystem = new Day12.CaveSystem(cavePairs);
+
+            var actualPathsCount = caveSystem.CountPathsThroughCaveSystemWithExtraVisit();
+
+            Assert.Equal(36, actualPathsCount);
+        }
+
+
+        [Theory]
+        [InlineData(0, 10)]
+        [InlineData(1, 19)]
+        [InlineData(2, 226)]
+        public void CorrectlyCalculatesSolutionPart1(int testDataNumber, int expectedPathsCount)
+        {
+            var inputData = GetTestData(testDataNumber);
+
+            var result = _day12Solution.CalculateSolution(Parts.Part1, inputData);
+
+            Assert.Equal(expectedPathsCount.ToString(), result);
+        }
+        
+        [Theory]
+        [InlineData(0, 36)]
+        [InlineData(1, 103)]
+        [InlineData(2, 3509)]
+        public void CorrectlyCalculatesSolutionPart2(int testDataNumber, int expectedPathsCount)
+        {
+            var inputData = GetTestData(testDataNumber);
+
+            var result = _day12Solution.CalculateSolution(Parts.Part2, inputData);
+
+            Assert.Equal(expectedPathsCount.ToString(), result);
+        }
+
+
+        private static string GetTestData(int dataNumber)
+        {
+            return dataNumber switch
+            {
+                0 => @"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end",
+                1 => @"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc",
+                _ => @"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"
+            };
+        }
+
+
+    }
+}
---
+++ AdventOfCode-2021/AdventOfCode.Csharp/Solutions/Day12.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Csharp.Solutions
+{
+    // Day 12: Passage Pathing (https://adventofcode.com/2021/day/12)
+    public class Day12 : IPuzzle
+    {
+        public string CalculateSolution(Parts part, string inputData)
+        {
+            var connectedCavePairs = inputData.Split(Environment.NewLine).Select(line => line.Split('-')).ToList();
+
+            return part switch
+            {
+                Parts.Part1 => $"{SolvePart1(connectedCavePairs)}",
+                Parts.Part2 => $"{SolvePart2(connectedCavePairs)}",
+                _ => throw new ArgumentOutOfRangeException(nameof(part), part, "There are only 2 parts.")
+            };
+        }
+        
+        private static int SolvePart1(IEnumerable<string[]> connectedCavePairs)
+        {
+            var caveSystem = new CaveSystem(connectedCavePairs);
+            return caveSystem.CountPathsThroughCaveSystem();
+        }
+
+        private static int SolvePart2(IEnumerable<string[]> connectedCavePairs)
+        {
+            var caveSystem = new CaveSystem(connectedCavePairs);
+            return caveSystem.CountPathsThroughCaveSystemWithExtraVisit();
+        }
+
+
+        private class Cave
+        {
+            public List<string> ConnectedCaves { get; } = new();
+        }
+
+        public class CaveSystem
+        {
+            private readonly Dictionary<string, Cave> _caves;
+            private int _pathsCount;
+            private List<string> _allPaths;
+
+            public CaveSystem(IEnumerable<string[]> connectedCavePairs)
+            {
+                _caves = new Dictionary<string, Cave>();
+                _allPaths = new List<string>();
+
+                foreach (var pair in connectedCavePairs)
+                {
+                    if (!_caves.ContainsKey(pair[0]))
+                        _caves[pair[0]] = new Cave();
+                    _caves[pair[0]].ConnectedCaves.Add(pair[1]);
+
+                    if (!_caves.ContainsKey(pair[1]))
+                        _caves[pair[1]] = new Cave();
+                    _caves[pair[1]].ConnectedCaves.Add(pair[0]);
+                }
+            }
+
+            public int CountPathsThroughCaveSystem()
+            {
+                const string starCave = "start";
+                _pathsCount = 0;
+                TraverseCountPaths(starCave, Enumerable.Empty<string>());
+                return _pathsCount;
+            }
+
+            public int CountPathsThroughCaveSystemWithExtraVisit()
+            {
+                const string starCave = "start";
+                _pathsCount = 0;
+                TraverseCountPathsWithExtraVisit(starCave, Enumerable.Empty<string>());
+                return _pathsCount;
+            }
+            
+            public List<string> FindAllPaths()
+            {
+                const string starCave = "start";
+                _allPaths = new List<string>();
+
+                TraversePaths(starCave, Enumerable.Empty<string>(), _allPaths);
+
+                return _allPaths;
+            }
+
+            public List<string> FindAllPathsWithExtraVisit()
+            {
+                const string starCave = "start";
+                _allPaths = new List<string>();
+
+                TraversePathsWithExtraVisit(starCave, Enumerable.Empty<string>(), _allPaths);
+
+                return _allPaths;
+            }
+
+            private void TraverseCountPaths(string currentCave, IEnumerable<string> path)
+            {
+                var currentPath = new List<string>(path) { currentCave };
+                if (currentCave != "end")
+                {
+                    var cave = _caves[currentCave];
+                    foreach (var connectedCave in cave.ConnectedCaves)
+                    {
+                        if (connectedCave.All(char.IsLower))
+                        {
+                            if (currentPath.Contains(connectedCave))
+                                continue;
+                        }
+
+                        TraverseCountPaths(connectedCave, currentPath);
+                    }
+                }
+                else
+                {
+                    _pathsCount++;
+                }
+            }
+
+            private void TraverseCountPathsWithExtraVisit(string currentCave, IEnumerable<string> path)
+            {
+                var currentPath = new List<string>(path) { currentCave };
+                if (currentCave != "end")
+                {
+                    var cave = _caves[currentCave];
+                    foreach (var connectedCave in cave.ConnectedCaves)
+                    {
+                        if (connectedCave.All(char.IsLower))
+                        {
+                            if (currentPath.Contains(connectedCave))
+                            {
+                                switch (connectedCave)
+                                {
+                                    case "start":
+                                    case "end":
+                                        continue;
+                                }
+
+                                if (currentPath.Any(caveName => caveName.All(char.IsLower) && currentPath.Count(caveInPath => caveInPath == caveName) > 1))
+                                    continue;
+                            }
+                        }
+                        TraverseCountPathsWithExtraVisit(connectedCave, currentPath);
+                    }
+                }
+                else
+                {
+                    _pathsCount++;
+                }
+            }
+
+            private void TraversePaths(string currentCave, IEnumerable<string> path, ICollection<string> allPaths)
+            {
+                var currentPath = new List<string>(path) { currentCave };
+                if (currentCave != "end")
+                {
+                    var cave = _caves[currentCave];
+                    foreach (var connectedCave in cave.ConnectedCaves)
+                    {
+                        if (connectedCave.Any(char.IsUpper) || !currentPath.Contains(connectedCave))
+                            TraversePaths(connectedCave, currentPath, allPaths);
+                    }
+                }
+                else
+                {
+                    allPaths.Add(string.Join(',', currentPath));
+                }
+            }
+
+            private void TraversePathsWithExtraVisit(string currentCave, IEnumerable<string> path, ICollection<string> allPaths)
+            {
+                var currentPath = new List<string>(path) { currentCave };
+                if (currentCave != "end")
+                {
+
+                    var cave = _caves[currentCave];
+                    foreach (var connectedCave in cave.ConnectedCaves)
+                    {
+                        if (connectedCave.All(char.IsLower) && currentPath.Contains(connectedCave))
+                        {
+                            switch (connectedCave)
+                            {
+                                case "start":
+                                case "end":
+                                    continue;
+                            }
+
+                            if (currentPath.Any(caveName => caveName.All(char.IsLower) && currentPath.Count(caveInPath => caveInPath == caveName) > 1))
+                                continue;
+                        }
+                        TraversePathsWithExtraVisit(connectedCave, currentPath, allPaths);
+                    }
+                }
+                else
+                {
+                    allPaths.Add(string.Join(',', currentPath));
+                }
+            }
+
+
+        }
+
+    }
+}
