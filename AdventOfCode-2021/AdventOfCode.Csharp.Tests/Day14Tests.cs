35b60c1784fa050371140f38a39722279b3d881e
Evaldas Naujanis <evaldas.naujanis@visma.com>
2022-02-03 01:28:16 +0200

    AoC#14: Day 14 - Extended Polymerization. Input data, C# solution, tests (part 1)

---
+++ AdventOfCode-2021/AdventOfCode.Csharp.Tests/Day14Tests.cs
@@ -0,0 +1,83 @@
+﻿using AdventOfCode.Csharp.Solutions;
+using Xunit;
+
+namespace AdventOfCode.Csharp.Tests
+{
+    public class Day14Tests
+    {
+        private readonly Day14 _day14Solution = new();
+
+        [Fact]
+        public void CorrectlyCalculatesSolutionPart1_After10Steps()
+        {
+            var inputData = GetTestData();
+
+            var diffBetweenMostAndLeastCommonElements = _day14Solution.CalculateSolution(Parts.Part1, inputData);
+
+            Assert.Equal("1588", diffBetweenMostAndLeastCommonElements);
+        }
+
+        [Fact]
+        public void CorrectlyCalculatesSolutionPart2_FragmentAfter40Steps()
+        {
+            var inputData = @"NN
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+
+            var diffBetweenMostAndLeastCommonElements = _day14Solution.CalculateSolution(Parts.Part2, inputData);
+
+            Assert.Equal("2188189693529", diffBetweenMostAndLeastCommonElements);
+        }
+
+        [Fact]
+        public void CorrectlyCalculatesSolutionPart2_After40Steps()
+        {
+            var inputData = GetTestData();
+
+            var diffBetweenMostAndLeastCommonElements = _day14Solution.CalculateSolution(Parts.Part2, inputData);
+
+            Assert.Equal("2188189693529", diffBetweenMostAndLeastCommonElements);
+        }
+
+
+        private static string GetTestData()
+        {
+            return @"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+
+        }
+
+
+    }
+}
---
+++ AdventOfCode-2021/AdventOfCode.Csharp/Solutions/Day14.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace AdventOfCode.Csharp.Solutions
+{
+    // Day 14: Extended Polymerization (https://adventofcode.com/2021/day/14)
+    public class Day14 : IPuzzle
+    {
+        public string CalculateSolution(Parts part, string inputData)
+        {
+            var data = inputData.Split(Environment.NewLine + Environment.NewLine);
+            var polymerTemplate = data[0];
+            var insertionRules = data[1].Split(Environment.NewLine)
+                .Select(rule => rule.Split(" -> "))
+                .Select(pair => (pair[0], pair[1][0])).ToList();
+
+            return part switch
+            {
+                Parts.Part1 => $"{SolvePart1(polymerTemplate, insertionRules, 10)}",
+                Parts.Part2 => $"{SolvePart1(polymerTemplate, insertionRules, 40)}",
+                _ => throw new ArgumentOutOfRangeException(nameof(part), part, "There are only 2 parts.")
+            };
+        }
+        
+        private static long SolvePart1(string polymerTemplate, IReadOnlyList<(string match, char insertChar)> insertionRules, int stepsCount)
+        {
+            var currentPolymer = polymerTemplate;
+            for (var step = 0; step < stepsCount; step++)
+            {
+                Console.WriteLine($"- - Step - {step} -");
+                Dictionary<int, char> insertions = new();
+                foreach (var (match, insertChar) in insertionRules)
+                {
+                    var index = -1;
+                    do
+                    {
+                        index = currentPolymer.IndexOf(match, index + 1, StringComparison.Ordinal);
+                        if (index > -1)
+                        {
+                            insertions[index+1] = insertChar;
+                        }
+                    } while (index > -1);
+                }
+
+                // apply insertions backwards
+                var polymerElements = currentPolymer.ToList();
+                foreach (var (index, insertChar) in insertions.OrderByDescending(ins => ins.Key))
+                {
+                    polymerElements.Insert(index, insertChar);
+                }
+
+                currentPolymer = new string(polymerElements.ToArray());
+
+                var statis = currentPolymer.GroupBy(ch => ch).Select(g => new
+                {
+                    ch = g.Key,
+                    cnt = g.Count()
+                });
+
+            }
+
+            var stats = currentPolymer.GroupBy(c => c).Select(g => (element: g.Key, count: g.LongCount())).ToList();
+            var mostCommonElementQuantity = stats.Max(e => e.count);
+            var leastCommonElementQuantity = stats.Min(e => e.count);
+
+            return mostCommonElementQuantity - leastCommonElementQuantity;
+        }
+
+        private static int SolvePart2(string polymerTemplate, IEnumerable<(string match, char insertChar)> insertionRules)
+        {
+            
+            return 0;
+        }
+        
+    }
+}
