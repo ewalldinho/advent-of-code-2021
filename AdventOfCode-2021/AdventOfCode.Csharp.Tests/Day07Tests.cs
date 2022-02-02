8e3e23c7ff82d3e294e298a5a094c95b11828912
Evaldas Naujanis <evaldas.naujanis@visma.com>
2022-02-03 00:32:17 +0200

    AoC#7: Day 7: The Treachery of Whales. Input data, C# solution and tests

---
+++ AdventOfCode-2021/AdventOfCode.Csharp.Tests/Day07Tests.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Csharp.Solutions;
+using Xunit;
+
+namespace AdventOfCode.Csharp.Tests
+{
+    public class Day07Tests
+    {
+        private readonly Day07 _day07Solution = new();
+
+        [Fact]
+        public void SolutionPart1Test()
+        {
+            var inputData = GetTestData();
+
+            var result = _day07Solution.CalculateSolution(Parts.Part1, inputData);
+
+            Assert.Equal("37", result);
+        }
+
+        [Fact]
+        public void SolutionPart2Test()
+        {
+            var inputData = GetTestData();
+
+            var result = _day07Solution.CalculateSolution(Parts.Part2, inputData);
+
+            Assert.Equal("168", result);
+        }
+
+
+        private static string GetTestData() =>
+            @"16,1,2,0,4,2,7,1,2,14";
+    }
+}
---
+++ AdventOfCode-2021/AdventOfCode.Csharp/Solutions/Day07.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Csharp.Solutions
+{
+    // Day 7: The Treachery of Whales (https://adventOfCode.com/2021/day/7)
+    public class Day07 : IPuzzle
+    {
+        public string CalculateSolution(Parts part, string inputData)
+        {
+            var crabHorizontalPositions = inputData.Split(',').Select(int.Parse).ToList();
+
+            return part switch
+            {
+                Parts.Part1 => $"{SolvePart1(crabHorizontalPositions)}",
+                Parts.Part2 => $"{SolvePart2(crabHorizontalPositions)}",
+                _ => throw new ArgumentOutOfRangeException(nameof(part), part, "There are only 2 parts.")
+            };
+        }
+
+        private static int SolvePart1(IReadOnlyCollection<int> horizontalPositions)
+        {
+            var min = horizontalPositions.Min();
+            var max = horizontalPositions.Max();
+            var minFuelUsed = int.MaxValue;
+            for (var i = min; i <= max; i++)
+            {
+                var sum = horizontalPositions.Select(x => x >= i ? x - i : i - x).Sum();
+                if (sum < minFuelUsed)
+                    minFuelUsed = sum;
+            }
+
+            return minFuelUsed;
+        }
+
+        private static int SolvePart2(IReadOnlyCollection<int> positions)
+        {
+            var min = positions.Min();
+            var max = positions.Max();
+            var minFuelUsed = int.MaxValue;
+            var moveCosts = PreCalculateCosts(max-min);
+            for (var i = min; i <= max; i++)
+            {
+                var sum = positions.Select(x => moveCosts[x >= i ? x - i : i - x]).Sum();
+                if (sum < minFuelUsed)
+                    minFuelUsed = sum;
+            }
+
+            return minFuelUsed;
+        }
+
+        private static Dictionary<int, int> PreCalculateCosts(int upToSteps)
+        {
+            var costs = new Dictionary<int, int>
+            {
+                [0] = 0
+            };
+            for (var i = 1; i <= upToSteps; i++)
+            {
+                costs[i] = costs[i - 1] + i;
+            }
+            return costs;
+        }
+    }
+}
