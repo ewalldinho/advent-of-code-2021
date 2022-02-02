a60b379d0b4684858cd678a2868d7537f6210bb7
Evaldas Naujanis <evaldas.naujanis@visma.com>
2022-02-03 00:28:47 +0200

    AoC#5: Day 5: Hydrothermal Venture. Input data, C# solution and tests

---
+++ AdventOfCode-2021/AdventOfCode.Csharp.Tests/Day05Tests.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.Csharp.Solutions;
+using Xunit;
+
+namespace AdventOfCode.Csharp.Tests
+{
+    public class Day05Tests
+    {
+        private readonly Day05 _day05Solution = new(); 
+
+        [Fact]
+        public void SolutionPart1Test()
+        {
+            var inputData = GetTestData();
+
+            var result = _day05Solution.CalculateSolution(Parts.Part1, inputData);
+
+            Assert.Equal("5", result);
+        }
+
+        [Fact]
+        public void SolutionPart2Test()
+        {
+            var inputData = GetTestData();
+
+            var result = _day05Solution.CalculateSolution(Parts.Part2, inputData);
+
+            Assert.Equal("12", result);
+        }
+
+
+        private string GetTestData() =>
+@"0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
+
+    }
+}
---
+++ AdventOfCode-2021/AdventOfCode.Csharp/Solutions/Day05.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Csharp.Solutions
+{
+    // Day 5: Hydrothermal Venture (https://adventOfCode.com/2021/day/5)
+    public class Day05 : IPuzzle
+    {
+        public string CalculateSolution(Parts part, string inputData)
+        {
+            return part switch
+            {
+                Parts.Part1 => $"{SolvePart1(inputData)}",
+                Parts.Part2 => $"{SolvePart2(inputData)}",
+                _ => throw new ArgumentOutOfRangeException(nameof(part), part, "There are only 2 parts.")
+            };
+        }
+
+        private int SolvePart1(string inputData)
+        {
+            var lines = ParseData(inputData);
+            var matrix = new int[1000, 1000];
+            foreach (var line in lines)
+            {
+                if (line.P1.X != line.P2.X && line.P1.Y != line.P2.Y)
+                    continue;
+
+                var (fromX, toX) = line.P1.X <= line.P2.X ? (line.P1.X, line.P2.X) : (line.P2.X, line.P1.X);
+                var (fromY, toY) = line.P1.Y <= line.P2.Y ? (line.P1.Y, line.P2.Y) : (line.P2.Y, line.P1.Y);
+
+                for (var row = fromY; row <= toY; row++)
+                {
+                    for (var col = fromX; col <= toX; col++)
+                    {
+                        matrix[row, col]++;
+                    }
+                }
+            }
+
+            var overlapCount = 0;
+            for (var r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (var c = 0; c < matrix.GetLength(1); c++)
+                {
+                    if (matrix[r, c] > 1)
+                        overlapCount++;
+                }
+            }
+
+            return overlapCount;
+        }
+
+        private int SolvePart2(string inputData)
+        {
+            var lines = ParseData(inputData);
+            var matrix = new int[1000, 1000];
+            foreach (var line in lines)
+            {
+                var isDiagonal = line.P1.X != line.P2.X && line.P1.Y != line.P2.Y;
+
+                if (isDiagonal)
+                {
+                    var deltaX = line.P1.X <= line.P2.X ? 1 : -1;
+                    var deltaY = line.P1.Y <= line.P2.Y ? 1 : -1;
+                    var diagonalRow = line.P1.Y;
+                    var diagonalCol = line.P1.X;
+                    var count = Math.Abs(line.P2.X - line.P1.X);
+                    while (count >= 0)
+                    {
+                        matrix[diagonalRow, diagonalCol]++;
+                        diagonalRow += deltaY;
+                        diagonalCol += deltaX;
+                        count--;
+                    }
+                }
+                else
+                {
+                    var (fromX, toX) = line.P1.X <= line.P2.X ? (line.P1.X, line.P2.X) : (line.P2.X, line.P1.X);
+                    var (fromY, toY) = line.P1.Y <= line.P2.Y ? (line.P1.Y, line.P2.Y) : (line.P2.Y, line.P1.Y);
+
+                    for (var row = fromY; row <= toY; row++)
+                    {
+                        for (var col = fromX; col <= toX; col++)
+                        {
+                            matrix[row, col]++;
+                        }
+                    }
+                }
+            }
+
+            var overlapCount = 0;
+            for (var r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (var c = 0; c < matrix.GetLength(1); c++)
+                {
+                    if (matrix[r, c] > 1)
+                        overlapCount++;
+                }
+            }
+
+            return overlapCount;
+        }
+
+
+        private List<Line> ParseData(string inputData)
+        {
+            var lines = inputData.Split(Environment.NewLine)
+                .Select(line => line.Split(" -> "))
+                .Select(coords => new[] { Point.Parse(coords[0]), Point.Parse(coords[1]) })
+                .Select(points => new Line(points[0], points[1]));
+            return lines.ToList();
+        }
+
+        private class Point
+        {
+            public int X { get; init; }
+            public int Y { get; init; }
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public static Point Parse(string xyData)
+            {
+                var xy = xyData.Split(',').Select(int.Parse).ToArray();
+                return new Point(xy[0], xy[1]);
+            }
+        }
+
+        private class Line
+        {
+            public Point P1 { get; init; }
+            public Point P2 { get; init; }
+
+            public Line(Point p1, Point p2)
+            {
+                P1 = p1;
+                P2 = p2;
+            }
+        }
+    }
+}
