7aaf2385d2870fe18178dab81834d6e0c1639a99
Evaldas Naujanis <evaldas.naujanis@visma.com>
2022-02-02 23:56:41 +0200

    AoC#1: Day 01 - F# solution

---
+++ AdventOfCode-2021/AdventOfCode.Fsharp/InputData/Day01Data.fs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Fsharp.InputData
+
+module Day01Data = 
+    let testInput = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+    let expectedResultPart1 = "7"
+    let expectedResultPart2 = "5"
---
+++ AdventOfCode-2021/AdventOfCode.Fsharp/Solutions/Day01.fs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Fsharp.Solutions
+
+open System
+open System.Linq
+open AdventOfCode.Fsharp.InputData
+open System.Collections.Generic
+
+module Day01 = 
+    // let testInput = Day01Data.testInput
+    let solvePart1 = fun inputData -> 
+        inputData
+        |> fun (text : string) -> text.Split(Environment.NewLine)
+        |> fun lines -> lines.Select(fun line -> line |> int)
+        |> fun values -> values.Aggregate((0, Int32.MaxValue), fun (counter, previousValue) value ->
+            if value > previousValue then (counter + 1, value)
+            else (counter, value))
+        |> fun (counter, lastValue) -> counter
+
+
+    let count3MeasureSlidingWindows (measurements : IEnumerable<int>) : int = 
+        let measures = List.ofSeq(measurements)
+        let mutable count = 0
+        let mutable lastSum = Int32.MaxValue
+        for i = 2 to measures.Count()-1 do
+            let newSum = measures.Item(i) + measures.Item(i-1) + measures.Item(i-2)
+            count <- if lastSum < newSum then count + 1
+                     else count
+            lastSum <- newSum
+        count
+    
+    let solvePart2 = fun inputData -> 
+        inputData
+        |> fun (text:string) -> text.Split(Environment.NewLine)
+        |> fun lines -> lines.Select(fun line -> line |> int)
+        |> fun values -> count3MeasureSlidingWindows values
+        |> fun (counter) -> counter
+   
