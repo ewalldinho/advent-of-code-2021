042c21af3c72c6a05cc554db9eb829c4fe7b4531
Evaldas Naujanis <evaldas.naujanis@visma.com>
2022-02-03 00:26:03 +0200

    AoC#4: Day 04: Giant Squid. Input data, C# solution and tests

---
+++ AdventOfCode-2021/AdventOfCode.Csharp.Tests/Day04Tests.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode.Csharp.Solutions;
+using Xunit;
+
+namespace AdventOfCode.Csharp.Tests
+{
+    public class Day04Tests
+    {
+        private readonly Day04 _day04Solution = new();
+
+        [Fact]
+        public void SolutionPart1Test()
+        {
+            var inputData = GetTestData();
+
+            var result = _day04Solution.CalculateSolution(Parts.Part1, inputData);
+
+            Assert.Equal("4512", result);
+        }
+
+        [Fact]
+        public void SolutionPart2Test()
+        {
+            var inputData = GetTestData();
+
+            var result = _day04Solution.CalculateSolution(Parts.Part2, inputData);
+
+            Assert.Equal("1924", result);
+        }
+
+
+
+        private static string GetTestData() =>
+@"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+        
+    }
+}
---
+++ AdventOfCode-2021/AdventOfCode.Csharp/Solutions/Day04.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Csharp.Solutions
+{
+    // Day 4: Giant Squid (https://adventOfCode.com/2021/day/4)
+    public class Day04 : IPuzzle
+    {
+        public string CalculateSolution(Parts part, string inputData)
+        {
+            return part switch
+            {
+                Parts.Part1 => SolvePart1(inputData),
+                Parts.Part2 => SolvePart2(inputData),
+                _ => throw new ArgumentOutOfRangeException(nameof(part), part, "There are only 2 parts.")
+            };
+        }
+
+        private static string SolvePart1(string inputData)
+        {
+            var bingo = ParseData(inputData);
+            bool isWinner;
+            List<BingoBoard> winners;
+            do
+            {
+                var luckyNumber = bingo.DrawNumber();
+                winners = bingo.MarkNumberOnBoards(luckyNumber);
+                isWinner = winners.Count > 0;
+            }
+            while (!isWinner);
+
+            var winningScore = 0;
+            foreach (var winningBoard in winners)
+            {
+                var score = winningBoard.CalcWinningScore();
+                if (score > winningScore)
+                    winningScore = score;
+            }
+            return $"{winningScore}";
+        }
+
+        private static string SolvePart2(string inputData)
+        {
+            var bingo = ParseData(inputData);
+            List<BingoBoard> winners;
+            var boardsCount = bingo.BoardsCount;
+            var winnersCount = 0;
+            do
+            {
+                var luckyNumber = bingo.DrawNumber();
+                winners = bingo.MarkNumberOnBoards(luckyNumber);
+                winnersCount += winners.Count;
+            }
+            while (winnersCount < boardsCount);
+
+            var winningBoard = winners.Last();
+            var winningScore = winningBoard.CalcWinningScore();
+            
+            return $"{winningScore}";
+        }
+
+
+        private class BingoBoard
+        {
+            private readonly List<int> _numberMatrix;
+            private readonly bool[,] _markedNumbersMatrix;
+            private int _lastNumber;
+            public bool HasWon { get; private set; }
+
+            private BingoBoard(List<int> numberMatrix)
+            {
+                _markedNumbersMatrix = new bool[5, 5];
+                _numberMatrix = numberMatrix;
+            }
+
+            public bool CheckNumber(int number)
+            {
+                var index = _numberMatrix.IndexOf(number);
+                if (index <= -1)
+                    return false;
+
+                var row = index / 5;
+                var col = index % 5;
+                _markedNumbersMatrix[row, col] = true;
+                HasWon = IsWinner(row, col);
+                if (HasWon)
+                    _lastNumber = number;
+
+                return HasWon;
+            }
+
+            public int CalcWinningScore()
+            {
+                if (!HasWon)
+                    throw new InvalidOperationException($"This board is not a winner.");
+
+                var sum = 0;
+                for (var row = 0; row < 5; row++)
+                {
+                    for (var col = 0; col < 5; col++)
+                    {
+                        if ( ! _markedNumbersMatrix[row, col])
+                            sum += _numberMatrix[row*5 + col];
+                    }
+                }
+
+                return sum * _lastNumber;
+            }
+
+            private bool IsWinner(int row, int col)
+            {
+                var count = 0;
+                for (var c = 0; c < 5; c++)
+                {
+                    if (_markedNumbersMatrix[row, c])
+                        count++;
+                    if (count == 5)
+                        return true;
+                }
+                count = 0;
+                for (var r = 0; r < 5; r++)
+                {
+                    if (_markedNumbersMatrix[r, col])
+                        count++;
+                    if (count == 5)
+                        return true;
+                }
+                return false;
+            }
+
+            public static BingoBoard Parse(string[] boardData)
+            {
+                var boardNumbers = string.Join(' ', boardData);
+                var numbers = boardNumbers.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => n.Trim()).ToArray();
+                if (numbers.Length != 25)
+                    throw new ArgumentException("Invalid bingo board data");
+                
+                var bingoBoard = new BingoBoard(numbers.Select(int.Parse).ToList());
+                return bingoBoard;
+            }
+
+        }
+
+        private class BingoGame
+        {
+            private readonly Queue<int> _drawnNumbers;
+            private readonly IEnumerable<BingoBoard> _boards;
+
+            public BingoGame(IEnumerable<int> drawnNumbers, IEnumerable<BingoBoard> boards)
+            {
+                _drawnNumbers = new Queue<int>(drawnNumbers);
+                _boards = boards;
+            }
+
+            public int BoardsCount => _boards.Count();
+
+            public int DrawNumber()
+            {
+                return _drawnNumbers.Dequeue();
+            }
+
+            public List<BingoBoard> MarkNumberOnBoards(int number)
+            {
+                var winnerList = new List<BingoBoard>();
+                foreach (var board in _boards.Where(b => !b.HasWon))
+                {
+                    var isWinner = board.CheckNumber(number);
+                    if (isWinner)
+                        winnerList.Add(board);
+                }
+
+                return winnerList;
+            }
+
+        }
+
+        private static BingoGame ParseData(string inputData)
+        {
+            var lines = inputData.Split(Environment.NewLine);
+            var numbers = lines[0].Split(',').Select(int.Parse);
+            var boards = lines.Skip(2).Chunk(6).Select(BingoBoard.Parse).ToList();
+            return new BingoGame(numbers, boards);
+        }
+
+    }
+
+    
+
+}
